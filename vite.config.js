0f69707476bd950f6ffb3f5be8be827997f8543d
Vignesh Mariappan <vigneshmv2312@gmail.com>
2024-04-21 19:45:56 +0530

    tic-tac-toe deploy changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Vignesh-Mariappan.github.io/react-machine-coding/tic-tac-toe/',
+  base: 'https://Vignesh-Mariappan.github.io/tic-tac-toe/',
 });
